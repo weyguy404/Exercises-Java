4de1407619041474ab749e0a56b1960bd5f221e6
Don Mitchell <dmitchell@placester.com>
2015-04-02 11:11:50 -0400

    Update unit test to check contains for non-existent item and to ensure dequeue pops items in order and the right number of items.

--- src/test/com/placester/test/PriorityQueueTest.java
+++ src/test/com/placester/test/PriorityQueueTest.java
@@ -6,23 +6,20 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PriorityQueueTest
 {
 
-    
-    
     @Test
     public void test() throws InterruptedException
     {
         final ThreadSafePriorityQueue<QueueTestTask> q = new ThreadSafePriorityQueue<>();
         final Random rand = new Random();
-        ExecutorService threadPool = Executors.newFixedThreadPool(20);
+        final ExecutorService threadPool = Executors.newFixedThreadPool(20);
 
-        int max = 1000;
+        final int max = 1000;
         for(int i = 0; i < max; i++)
         {
             final int idx = i;
@@ -30,34 +27,39 @@ public class PriorityQueueTest
                 @Override
                 public void run()
                 {
-                    q.add(new Priority<QueueTestTask>(rand.nextInt(100), new QueueTestTask(idx)));
                     try
                     {
                         //make sure we actually hit multiple threads;
-                        Thread.sleep(50);
-                    } catch (InterruptedException e)
+                        Thread.sleep(rand.nextInt(50));
+                        q.add(new Priority<QueueTestTask>(rand.nextInt(100), new QueueTestTask(idx)));
+                    } catch (final InterruptedException e)
                     {
-                        // TODO Auto-generated catch block
                         throw new RuntimeException(e);
                     }
                 }});
         }
-        
+
         threadPool.awaitTermination(10000, TimeUnit.MILLISECONDS);
-        
-        int startPriority = 0;
-        //Test that threadsafe insertion worked
+
+        Assert.assertFalse(q.contains(new Priority<QueueTestTask>(101, new QueueTestTask(1111))));
+
+        int lastPriority = 0, count=0;
+        //Test that threadsafe insertion worked and things return in right order
         while(!q.isEmpty())
         {
-            Priority<QueueTestTask> item = q.poll();
-            Assert.assertTrue(item.priority >= startPriority);
+            final Priority<QueueTestTask> item = q.poll();
+            Assert.assertTrue(item.priority >= lastPriority);
+            lastPriority = item.priority;
+            count++;
         }
-        
-        Priority<QueueTestTask> item1 = new Priority<QueueTestTask>(666, new QueueTestTask(0));
-        Priority<QueueTestTask> item2 = new Priority<QueueTestTask>(0, new QueueTestTask(0));
-        Priority<QueueTestTask> item3 = new Priority<QueueTestTask>(42, new QueueTestTask(0));
+        // ensure we dequeued as many items as we inserted
+        Assert.assertEquals(count, max);
 
-        
+        final Priority<QueueTestTask> item1 = new Priority<QueueTestTask>(666, new QueueTestTask(0));
+        final Priority<QueueTestTask> item2 = new Priority<QueueTestTask>(0, new QueueTestTask(0));
+        final Priority<QueueTestTask> item3 = new Priority<QueueTestTask>(42, new QueueTestTask(0));
+
+
         q.add(item1);
         q.add(item2);
         q.add(item3);
@@ -70,7 +72,7 @@ public class PriorityQueueTest
         item = q.poll();
         Assert.assertEquals(item, item1);
 
-        
+
         q.add(item1);
         Assert.assertFalse(q.isEmpty());
         item = q.peek();
